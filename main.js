49c90b2b9e66e05f91a5c4e7b1f6eca256677272
vema15 <140360041+vema15@users.noreply.github.com>
2023-10-27 16:28:14 -0700

    Single digit calculations now function. Any multi-digit calculations and calculations w/ decimals will not compute. Note: Need to fix how the array is split upon calculation (concat if index to right is num or decimal, else do not concat)

--- main.js
+++ main.js
@@ -39,126 +39,223 @@
 
     let multBtn = document.querySelector('.mult-btn');
     multBtn.addEventListener('click', appendMult);
-    
+
     let divBtn = document.querySelector('.division-btn');
     divBtn.addEventListener('click', appendDiv);
 
 //Functional Button Event Listeners
-let clrBtn = document.querySelector('.clear-btn');
-clrBtn.addEventListener('click', clear);
+    let clrBtn = document.querySelector('.clear-btn');
+    clrBtn.addEventListener('click', clear);
 
+    let decimalButton = document.querySelector('.decimal-btn');
+    decimalButton.addEventListener('click', appendDecimal);
+
+    let percentButton = document.querySelector('.percent-btn');
+    percentButton.addEventListener('click', appendPercent);
+
+    let negButton = document.querySelector('.neg-sign-btn');
+    negButton.addEventListener('click', appendNegative);
 
 
 
 
 
 //Append Numbers
-function appendZero() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=0;
-    } else {
-        displayBox.innerText += 0;
+    function appendZero() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 0;
+        } else {
+            displayBox.innerText += 0;
+        }
     }
-}
-function appendOne() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=1;
-    } else {
-        displayBox.innerText += 1;
+
+    function appendOne() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 1;
+        } else {
+            displayBox.innerText += 1;
+        }
     }
-}
-function appendTwo() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=2;
-    } else {
-        displayBox.innerText += 2;
+
+    function appendTwo() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 2;
+        } else {
+            displayBox.innerText += 2;
+        }
     }
-}
-function appendThree() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=3;
-    } else {
-        displayBox.innerText += 3;
+
+    function appendThree() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 3;
+        } else {
+            displayBox.innerText += 3;
+        }
     }
-}
-function appendFour() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=4;
-    } else {
-        displayBox.innerText += 4;
+
+    function appendFour() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 4;
+        } else {
+            displayBox.innerText += 4;
+        }
     }
-}
-function appendFive() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=5;
-    } else {
-        displayBox.innerText += 5;
+
+    function appendFive() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 5;
+        } else {
+            displayBox.innerText += 5;
+        }
     }
-}
-function appendSix() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=6;
-    } else {
-        displayBox.innerText += 6;
+
+    function appendSix() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 6;
+        } else {
+            displayBox.innerText += 6;
+        }
     }
-}
-function appendSeven() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=7;
-    } else {
-        displayBox.innerText += 7;
+
+    function appendSeven() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 7;
+        } else {
+            displayBox.innerText += 7;
+        }
     }
-}
-function appendEight() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=8;
-    } else {
-        displayBox.innerText += 8;
+
+    function appendEight() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 8;
+        } else {
+            displayBox.innerText += 8;
+        }
     }
-}
-function appendNine() {
-    let displayBox = document.querySelector('.row-1-display');
-    if (displayBox.innerText == '0') {
-        displayBox.innerText=9;
-    } else {
-        displayBox.innerText += 9;
+
+    function appendNine() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = 9;
+        } else {
+            displayBox.innerText += 9;
+        }
     }
-}
 
 //Append Operators
-function appendAdd() {
-    let displayBox = document.querySelector('.row-1-display');
+    function appendAdd() {
+        let displayBox = document.querySelector('.row-1-display');
         displayBox.innerText += '+';
-}
+    }
 
-function appendSubtract() {
-    let displayBox = document.querySelector('.row-1-display');
+    function appendSubtract() {
+        let displayBox = document.querySelector('.row-1-display');
         displayBox.innerText += '-';
-}
+    }
 
-function appendMult() {
-    let displayBox = document.querySelector('.row-1-display');
+    function appendMult() {
+        let displayBox = document.querySelector('.row-1-display');
         displayBox.innerText += '×';
-}
+    }
 
-function appendDiv() {
-    let displayBox = document.querySelector('.row-1-display');
+    function appendDiv() {
+        let displayBox = document.querySelector('.row-1-display');
         displayBox.innerText += '÷';
-}
+    }
 
 
 //Append Functional Buttons
-function clear() {
-    let displayBox = document.querySelector('.row-1-display');
-    displayBox.innerText = 0;
+    function clear() {
+        let displayBox = document.querySelector('.row-1-display');
+        displayBox.innerText = 0;
 
-}
+    }
+
+    function appendDecimal() {
+        let displayBox = document.querySelector('.row-1-display');
+        displayBox.innerText += '.';
+    }
+
+    function appendPercent() {
+        let displayBox = document.querySelector('.row-1-display');
+        displayBox.innerText += '%';
+    }
+
+    function appendNegative() {
+        let displayBox = document.querySelector('.row-1-display');
+        if (displayBox.innerText == '0') {
+            displayBox.innerText = '-';
+        } else {
+            displayBox.innerText += '-';
+        }
+    }
+
+
+
+    //Operation Function
+
+    let equalBtn = document.querySelector('.equal-btn');
+    equalBtn.addEventListener('click', runCalc);
+
+    function runCalc() {
+        let displayBox = document.querySelector('.row-1-display');
+        let displayBoxArray = displayBox.innerText.split('');
+
+
+
+        for (let i = 0; i <= displayBoxArray.length - 1; i++) {
+            if (displayBoxArray[i] == '×') {
+                displayBoxArray[i] = multiply(displayBoxArray, i);
+                displayBoxArray.splice((i + 1), 1);
+                displayBoxArray.splice((i - 1), 1);
+                i = 0;
+            } else if (displayBoxArray[i] == '÷') {
+                displayBoxArray[i] = divide(displayBoxArray, i);
+                displayBoxArray.splice((i + 1), 1);
+                displayBoxArray.splice((i - 1), 1);
+                i = 0;
+            } else if (displayBoxArray[i] == '+') {
+                displayBoxArray[i] = add(displayBoxArray, i);
+                displayBoxArray.splice((i + 1), 1);
+                displayBoxArray.splice((i - 1), 1);
+                i = 0;
+            } else if (displayBoxArray[i] == '-') {
+                displayBoxArray[i] = subtract(displayBoxArray, i);
+                displayBoxArray.splice((i + 1), 1);
+                displayBoxArray.splice((i - 1), 1);
+                i = 0;
+
+            }
+
+        }
+
+        displayBox.innerText = displayBoxArray;
+
+
+        //Operations
+            function multiply(displayBoxArray, i) {
+                return (parseInt(displayBoxArray[i - 1]) * parseInt(displayBoxArray[i + 1]));
+            }
+
+            function divide(displayBoxArray, i) {
+                return (parseInt(displayBoxArray[i - 1]) / parseInt(displayBoxArray[i + 1]));
+            }
+
+            function add(displayBoxArray, i) {
+                return (parseInt(displayBoxArray[i - 1])) + (parseInt(displayBoxArray[i + 1]));
+            }
+
+            function subtract(displayBoxArray, i) {
+                return (parseInt(displayBoxArray[i - 1])) - (parseInt(displayBoxArray[i + 1]));
+            }
+    }
+    
